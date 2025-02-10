4223f5c458fdeaab9f47f31204a0e4ab7dcfb832
Estudiantes DCI <ge.ramirezcastillo@ugto.mx>
2025-02-10 11:50:10 -0600

    Actividad de clase numero cuatro

---
+++ Feb10/actividad_programa.c
@@ -0,0 +1,10 @@
+/*Creado el 10 de Febrero del 2025 por Kitzia Pérez*/
+#include<stdio.h>
+int main()
+{
+  printf("Hola Kitzia \n");
+  printf("Este programa es una actividad de la cuarta clase\n");
+  printf("Recuerda practicar en la casa porfa\n");
+}
+/*Termine de escribir el programa*/
+
