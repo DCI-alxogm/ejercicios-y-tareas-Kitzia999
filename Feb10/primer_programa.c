8d56e3e909f4f80d846e387096c9e7aa20b6cc01
Estudiantes DCI <ge.ramirezcastillo@ugto.mx>
2025-02-10 11:24:24 -0600

    Realice mi primer programa

---
+++ Feb10/primer_programa.c
@@ -0,0 +1,9 @@
+/*Creado el 10 de Febrero de 2025 por Kitzia Fernanda Perez Martinez*/
+#include<stdio.h>
+int main()
+{
+  printf("Hola Kitzia \n");
+  printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
