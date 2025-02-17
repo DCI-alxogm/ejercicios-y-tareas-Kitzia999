746b74a3ef05e59d156bbfe2375b128537e8992d
Estudiantes DCI <kf.perezmartinez@ugto.mx>
2025-02-17 10:50:26 -0600

    programa de prueba en una nueva computadora

---
+++ Feb_Prueba_NC/archivo_de_prueba.c
@@ -0,0 +1,9 @@
+/*Creado el 17 de febrero del 2025 por Kitzia Perez*/
+#include<stdio.h>
+int main()
+{
+ printf("hola kitzia \n");
+ printf("Este es un archivo de prueba en una nueva computadora\n");
+}
+/*Termine de escribir el programa*/
+
---
+++ archivo_de_prueba.c
@@ -0,0 +1,9 @@
+/* Creado el 17 de Febrero del 2025 por KItzia Perez*/
+#include<stdio.h>
+int main()
+{
+ printf("Hola Kitzia \n");
+ printf("Este es un programa de prueba en la nueva computadora\n");
+}
+/*Termine de escribir el programa*/
+
