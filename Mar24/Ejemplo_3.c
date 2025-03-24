19b32d91da9f9428411a46c3b8344409b3aec568
Estudiantes DCI <kf.perezmartinez@ugto.mx>
2025-03-24 11:38:07 -0600

    Ejemplo while 3

---
+++ Mar24/Ejemplo_3.c
@@ -0,0 +1,31 @@
+/*
+Creado el 24 de marzo del 2025
+*/
+
+#include<stdio.h>
+
+int main() {
+   
+   float temp_C, temp_K;
+   float inicial = 100, final = 200, delta;
+   int n = 10;
+   char op[2];
+   
+   op[0] = 's';
+   delta = (final - inicial) / n;
+   
+   while (op[0] == 's') {
+       temp_K = 0;
+       temp_C = inicial;
+   
+   while (temp_C < final) {
+        temp_K = temp_C + 273.15;
+        printf("%f %f\n", temp_C, temp_K);
+        temp_C = temp_C + delta;
+       }
+       
+       printf("¿Deseas hacer otra operación? (s/n)\n");
+       scanf("%1s", op);  // Limitar a un solo carácter para evitar problemas
+   }
+   return 0;
+}
