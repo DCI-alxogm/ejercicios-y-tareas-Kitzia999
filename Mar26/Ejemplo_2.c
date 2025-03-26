e6737cb13cc2c8bf82728096d663576166f4f7c3
Estudiantes DCI <kf.perezmartinez@ugto.mx>
2025-03-26 11:04:30 -0600

    Ejemplo numero 2

---
+++ Mar26/Ejemplo_2.c
@@ -0,0 +1,21 @@
+/*
+Creado el 26 de Marzo
+Ejemplo 2
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main ()
+{
+    float exp_;
+  int n=10,j;
+  
+    for(j=0;j<n;j++){
+        exp_=exp(j);
+        printf("%i\t %f\n", j, exp_);
+    }
+    
+    return (0);
+}    
+
