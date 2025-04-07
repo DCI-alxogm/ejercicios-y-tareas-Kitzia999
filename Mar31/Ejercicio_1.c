4f3ff50a2f9939a192cada57ae55f3a61785df68
Estudiantes DCI <kf.perezmartinez@ugto.mx>
2025-04-07 09:54:57 -0600

    Ejercicio 1

--- Mar31/Ejercicio_1.c
+++ Mar31/Ejercicio_1.c
@@ -6,8 +6,11 @@ Creado el 31 de Marzo del 2025
 
 int main(){
         int numero;
+        char opcion;
         
         //Pedir al usuario que ingrese un numero
+        
+     do{
         do {
             printf("Ingrese un numero positivo: ");
             scanf("%d", &numero);
@@ -18,6 +21,14 @@ int main(){
         }while (numero <=0);
         
         printf("Numero valido ingresado: %d\n", numero);
+        
+        //Preguntar si desea repetir
+        printf("¿Deseas ingresar otro numero? (s/n): ");
+        scanf(" %c", &opcion); //Espacio antes de %c para evitar problemas con el buffer
+        
+    }while (opcion == 's' || opcion == 'S');
+    
+        printf("Saliendo del programa.\n");
         
         return 0;
 }
