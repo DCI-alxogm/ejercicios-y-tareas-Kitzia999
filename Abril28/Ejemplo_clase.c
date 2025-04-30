ada9ba73ea15bfe17bcc7cfe3568f5b097339e5f
Estudiantes DCI <kf.perezmartinez@ugto.mx>
2025-04-30 10:35:31 -0600

    Ejemplo de la clase del 28 de abril

---
+++ Abril28/Ejemplo_clase.c
@@ -0,0 +1,24 @@
+/*
+Creado el 28 de abril de 2025
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   int n_filas = 3, m_cols = 3;
+   int i, j;
+   float array_2d[3][3]; // Declaración del arreglo
+   for (i = 0; i < n_filas; i++) {
+       for (j = 0; j < m_cols; j++) {
+           scanf("%f", &array_2d[i][j]);
+       }
+   }
+   for (i = 0; i < n_filas; i++) {
+       for (j = 0; j < m_cols; j++) {
+           printf("%f\t", array_2d[i][j]);
+       }
+       printf("\n");
+   }
+   return 0;
+}
